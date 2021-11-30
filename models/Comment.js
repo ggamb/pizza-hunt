15c3dc9ad0e1a6f3f9d5be48b1993b9941f18a11
ggamb <glenngambardella372@gmail.com>
2021-11-30 18:30:05 -0500

    minor updates

--- models/Comment.js
+++ models/Comment.js
@@ -1,29 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema(
-    {
-        writtenBy: {
-            type: String
-        },
-        commentBody: {
-            type: String
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now
-        },
-        // use ReplySchema to validate data for a reply
-        replies: [ReplySchema]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-);
 
 const ReplySchema = new Schema(
     {
@@ -50,6 +27,34 @@ const ReplySchema = new Schema(
         }
     }
 );
+
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+
 
 CommentSchema.virtual('replyCount').get(function () {
     return this.replies.length;
