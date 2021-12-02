1867e89cebef198cc49bbf6b54c785b4dff125c3
ggamb <glenngambardella372@gmail.com>
2021-12-02 12:37:51 -0500

    added comment and reply validation

--- models/Comment.js
+++ models/Comment.js
@@ -10,10 +10,13 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
         },
         createdAt: {
             type: Date,
@@ -35,7 +38,8 @@ const CommentSchema = new Schema(
             type: String
         },
         commentBody: {
-            type: String
+            type: String,
+            required: true,
         },
         createdAt: {
             type: Date,
